3a54c09aa81939a2431a95a56bcef2cd06ae358c
= <=>
2022-03-24 20:08:57 -0300

    Alterado

---
+++ calculadora.php
@@ -0,0 +1,21 @@
+<?php
+
+class Calculadora {
+
+    public static function soma($iNum1, $iNum2){
+        return $iNum1 + $iNum2;
+    }
+
+    public static function subrtracao($iNum1, $iNum2){
+        return $iNum1 - $iNum2;
+    }
+
+    public static function divisao($iNum1, $iNum2){
+        return $iNum1 / $iNum2;
+    }
+
+    public static function multiplicacao($iNum1, $iNum2){
+        return $iNum1 * $iNum2;
+    }
+
+}
---
+++ processa_dados.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once 'calculadora.php';
+
+class processaDados {
+    
+    public function __construct() {
+        $this->processaDados();
+    }
+    
+    private function processaDados() {
+        foreach ($_POST as $sKey => $sValue) {
+            if(!$this->isNumero($sValue) && (!isset($_SESSION['valor_visor']) || is_null($_SESSION['valor_visor']))){
+                    return;
+            }
+
+            if(isset($_SESSION['reset']) && $_SESSION['reset']){
+                $this->isNumero($sValue) ? $_SESSION['valor_visor'] = null : true;
+                $this->isNumero($sValue) ? $_SESSION['valor1'] = null : true;
+                $_SESSION['reset'] = false;
+            } 
+
+            if($sKey == 'limpar'){
+                $_SESSION['valor_visor'] = null;
+                $_SESSION['operador'] = null;
+            } else if($sKey === 'igual'){
+                $this->calcula();
+                $_SESSION['valor_visor'] = $_SESSION['valor1'];
+            } else {
+                isset($_SESSION['valor_visor']) ? $_SESSION['valor_visor'] .= $sValue : $_SESSION['valor_visor'] = $sValue;
+                if(!$this->isNumero($sValue)){
+                    $_SESSION['operador'] = $sValue;
+                } else if(!isset($_SESSION['operador']) || is_null($_SESSION['operador'])){
+                    $_SESSION['valor1'] = $_SESSION['valor_visor'];
+                } else {
+                    isset($_SESSION['valor2'])? $_SESSION['valor2'].= $sValue : $_SESSION['valor2'] = $sValue;
+                }
+            }
+        }
+    }
+    
+    private function calcula() {
+        switch (true) {
+            case $_SESSION['operador'] === '+':
+                $_SESSION['valor1'] = Calculadora::soma($_SESSION['valor1'], $_SESSION['valor2']);
+                break;
+            case $_SESSION['operador'] === '-':
+                $_SESSION['valor1'] = Calculadora::subrtracao($_SESSION['valor1'], $_SESSION['valor2']);
+                break;
+            case $_SESSION['operador'] === 'X':
+                $_SESSION['valor1'] = Calculadora::multiplicacao($_SESSION['valor1'], $_SESSION['valor2']);
+                break;
+            case $_SESSION['operador'] === '%':
+                $_SESSION['valor1'] = Calculadora::divisao($_SESSION['valor1'], $_SESSION['valor2']);
+                break;
+        }
+        $_SESSION['reset'] = true;
+        $_SESSION['operador'] = null;
+        $_SESSION['valor2'] = null;
+    }
+    
+    private function isNumero($iNumero) {
+        $aNumeros = ['zero', 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        return in_array($iNumero, $aNumeros);
+    }
+
+    public function __destruct() {
+        header('Location: index.php');
+    }
+    
+}
